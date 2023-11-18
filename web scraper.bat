5af7426b1fd4c985ff572101faa9555c209cb88b
Francis-sharon <66363189+Francis-sharon@users.noreply.github.com>
2023-11-18 20:24:12 +0530

    Add files via upload

---
+++ web scraper.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pip install --upgrade pip
+
+pip install os sys socks socket requests platform bs4 art datetime nltk pyqt5
+
+
+
+start python "C:\Users\JR.SHARON\Desktop\XENON\intro.py"
+
+timeout /t 5 /nobreak
+
+start python "C:\Users\JR.SHARON\Desktop\XENON\xenon.py"
+pause
